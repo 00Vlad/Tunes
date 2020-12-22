76ee948401abd8a9c9d187c470614f747488f985
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-12-23 00:36:43 +0100

    The video player module was made

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,21 +1,3 @@
 export const radioPlayerInit = () => {
   console.log('radio player');
 }
-
-const playerBtn = document.querySelectorAll('.player-btn')
-const playerBlock = document.querySelectorAll('.player-block')
-const temp = document.querySelector('.temp')
-
-const deactivationPlayer = () => {
-  temp.style.display = 'none'
-  playerBtn.forEach(item => item.classList.remove('active'))
-  playerBlock.forEach(item => item.classList.remove('active'))
-}
-
-playerBtn.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    deactivationPlayer()
-    btn.classList.add('active')
-    playerBlock[index].classList.add('active')
-  })
-})
